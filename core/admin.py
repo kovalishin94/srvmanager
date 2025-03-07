601413db1a95ae925986cf3c157fffadd676a2ae
kovalishin94 <kovalish94@mail.ru>
2025-03-07 13:13:54 +0700

    CredentialAdminForm

--- core/admin.py
+++ core/admin.py
@@ -1,15 +1,35 @@
+from django import forms
 from django.contrib import admin
 
 from .models import SSHCredential, WinRMCredential, Host
 
 
+class CredentialAdminForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(), required=False)
+
+    class Meta:
+        model = SSHCredential
+        fields = '__all__'
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        password = self.cleaned_data.get('password')
+        if password:
+            instance.set_password(password)
+        if commit:
+            instance.save()
+        return instance
+
+
 @admin.register(SSHCredential)
 class SSHCredentialAdmin(admin.ModelAdmin):
+    form = CredentialAdminForm
     list_display = ['id', 'username', '_password', 'port']
 
 
 @admin.register(WinRMCredential)
 class WinRMCredentialAdmin(admin.ModelAdmin):
+    form = CredentialAdminForm
     list_display = ['id', 'username', '_password', 'port', 'ssl']
 
 
