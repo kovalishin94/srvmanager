23d47b6ff0190d38f3aead63d65883b0d668ba50
kovalishin94 <kovalish94@mail.ru>
2025-03-07 11:47:54 +0700

    Add models to admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SSHCredential, WinRMCredential, Host
+
+
+@admin.register(SSHCredential)
+class SSHCredentialAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', '_password', 'port']
+
+
+@admin.register(WinRMCredential)
+class WinRMCredentialAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', '_password', 'port', 'ssl']
+
+
+@admin.register(Host)
+class HostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'os', 'ssh_credential', 'winrm_credential']
