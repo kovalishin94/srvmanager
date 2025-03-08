459dcc7e23b8228d967cd91b0b9751aa7691f676
kovalishin94 <kovalish94@mail.ru>
2025-03-08 19:51:32 +0700

    Add core/serializers.py

---
+++ core/serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+
+from .models import Host, SSHCredential, WinRMCredential
+
+
+class HostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Host
+        fields = ['id', 'name', 'ip', 'os']
+
+
+class CredentialSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = None
+        fields = ['id', 'username', 'password', 'host']
+
+    def create(self, validated_data: dict):
+        password = validated_data.pop('password')
+        instance = self.Meta.model(**validated_data)
+        instance.set_password(password)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data: dict):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
+
+class SSHCredentialSerializer(CredentialSerializer):
+    class Meta(CredentialSerializer.Meta):
+        model = SSHCredential
+        fields = CredentialSerializer.Meta.fields + \
+            ['port', 'ssh_key', 'passphrase']
+
+
+class WinRMCredentialSerializer(CredentialSerializer):
+    class Meta(CredentialSerializer.Meta):
+        model = WinRMCredential
+        fields = CredentialSerializer.Meta.fields + ['port', 'ssl']
