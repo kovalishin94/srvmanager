6d784b71be4fecdc3864cba81040869c6a98ffcb
kovalishin94 <kovalish94@mail.ru>
2025-08-18 21:32:51 +0700

    Add check action to EtalonInstanceViewSet for executing commands asynchronously

--- etaupdater/views.py
+++ etaupdater/views.py
@@ -1,14 +1,24 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from core.views import CorePageNumberPagination
 from .models import EtalonInstance, UpdateFile, EtalonUpdate
 from .serializers import EtalonInstancesSerializer, UpdateFileSerializer, EtalonUpdateSerializer
+from .tasks import check_execute_command
 
 
 class EtalonInstanceViewSet(viewsets.ModelViewSet):
     queryset = EtalonInstance.objects.all().order_by('-created_at')
     serializer_class = EtalonInstancesSerializer
     pagination_class = CorePageNumberPagination
+
+    @action(methods=["POST"], detail=True)
+    def check(self, request, pk=None):
+        instance = self.get_object()
+        execute_command = instance.create_execute_command()
+        check_execute_command.apply_async(args=[execute_command.id, instance.id], countdown=10)
+        return Response(status=status.HTTP_200_OK)
 
 
 class UpdateFileViewSet(mixins.CreateModelMixin,
